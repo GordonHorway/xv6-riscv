324691cc63321a87c54ae30626ee8c640cb8099b
Gordon Horway <ghorway@cogeco.ca>
2025-05-12 18:15:56 -0400

    Successfully integrated mem command

--- user/mem.c
+++ user/mem.c
@@ -1,23 +1,15 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
-#define NULL 0L
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]){
     int *p = malloc(sizeof(int));
-    if(p == NULL){
-        printf(2, "Error allocating memory\n");
-        exit();
-    }
-    // assert(p != NULL);
-    printf(1, "(%d) address pointed to by p: %p\n", getpid(), p);
-    // printf("(%d) address pointed to by p: %p\n", getpid(), p);
-    *p = 0; // a3
-    while (1) {
-        sleep(1);
+    printf("(pid: %d) address pointed to by p: %p\n", getpid(), p);
+    *p = 0;
+    for (int i = 0; i < 10; i++) {
+        sleep(20);
         *p = *p + 1;
-        printf(1, "(%d) p: %d\n", getpid(), *p);
-        // printf("(%d) p: %d\n", getpid(), *p);
+        printf("(pid: %d) content of p: %d\n", getpid(), *p);
     }
-    exit();
+    exit(0);
 }
