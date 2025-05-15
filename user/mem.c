c20729662960b577260711f433e9740f825d0b84
Gordon Horway <ghorway@cogeco.ca>
2025-05-15 00:15:56 -0400

    modified mem user function to accept parameter for integer input to increment

--- user/mem.c
+++ user/mem.c
@@ -2,10 +2,19 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
+/*
+    Maybe I should modify this code so that it can take input from CLI
+    *p = atoi(argv[1]);
+*/
+
 int main(int argc, char *argv[]){
     int *p = malloc(sizeof(int));
+    if(argc != 2){
+        fprintf(2, "mem: not enough arguments\n");
+        exit(1);
+    }
     printf("(pid: %d) address pointed to by p: %p\n", getpid(), p);
-    *p = 0;
+    *p = atoi(argv[1]);
     for (int i = 0; i < 10; i++) {
         sleep(20);
         *p = *p + 1;
