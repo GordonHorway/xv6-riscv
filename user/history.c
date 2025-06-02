97e224f4a7a3f2929d68da51d2efa4c101e503d0
Gordon Horway <ghorway@cogeco.ca>
2025-06-02 18:02:02 -0400

    added a check for anything after entering history command

--- user/history.c
+++ user/history.c
@@ -4,6 +4,11 @@
 
 int main(int argc, char **argv){
 
+    if(argc > 1){
+        fprintf(2, "command usage: history\n");
+        exit(1);
+    }
+
     char *args[] = {"cat", "sh_history", 0};
 
     if(exec(args[0], args) < 0){
