e35fb6e6f08d49e87e9cbe7b6288eb1099e62aa7
Gordon Horway <ghorway@cogeco.ca>
2025-06-05 23:25:08 -0400

    dont need error checking for getppid() I guess

--- user/getppid.c
+++ user/getppid.c
@@ -6,13 +6,7 @@ int main(int argc, char **argv){
 
     int ppid = getppid();
 
-    // add return value for indication of failure??
-    if(ppid >= 0){
     printf("parent process ID of getppid user command process %d\n", ppid);
-    } else {
-        fprintf(2, "There was an error getting the ppid of the current process!\n");
-        exit(1);
-    }
 
     exit(0);
 }
