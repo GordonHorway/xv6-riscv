fdc0710b068b18d44c9ba4ac0b6ec1f22aa87189
Gordon Horway <ghorway@cogeco.ca>
2025-07-02 23:33:46 -0400

    added p->priority = 0; to proc.c allocproc function

--- kernel/proc.c
+++ kernel/proc.c
@@ -125,6 +125,7 @@ allocproc(void)
 found:
   p->pid = allocpid();
   p->state = USED;
+  p->priority = 0;
 
   // Allocate a trapframe page.
   if((p->trapframe = (struct trapframe *)kalloc()) == 0){
